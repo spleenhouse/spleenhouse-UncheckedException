38ad83345d0fe596847096b82008c13b5a63bbb1
spleenhouse <michellepozen@gmail.com>
2023-02-28 00:38:53 +0000

    Completed Lab Unchecked Exceptions

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,6 +14,7 @@ public class Lab {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        int x = 5/0;
+        System.out.println(x);
     }
 }
